49956863b2af54fa31d9ab2933c02c0bb34c72e0
Cody Condon <codycondon50@gmail.com>
2016-03-28 13:27:04 -0400

    set up connect between react redux set up mapstatetoprops

---
+++ src/containers/book-list.js
@@ -0,0 +1,33 @@
+import React, {Component} from 'react';
+// import connect function from react redux, which is the glue between react and redux
+import { connect } from 'react-redux';
+
+class BookList extends Component {
+  renderList() {
+    return this.props.books.map((book) => {
+      return (
+        <li key={book.title} className="list-group-item">{book.title}</li>
+      );
+    });
+  }
+  render() {
+    return (
+      <ul className = "list-group col-sm-4">
+      {this.renderList()}
+      </ul>
+    )
+  }
+}
+//mapstatetoprops will take in application state and set props to the component in this case its books,
+//which is reffered to above in props.books
+//VERY IMPORTANT FUNCTION IN REACT AND REDUX
+//when application state changes, BookList will automatically re-render 
+function mapStateToProps(state) {
+  // Whatever is returned will show up as props
+  //inside of BookList
+  return {
+    books: state.books
+  };
+}
+// connect takes a function and component and produces a container
+export default connect(mapStateToProps)(BookList);
