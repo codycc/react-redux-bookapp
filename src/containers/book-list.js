d44979e04dea5f0af5d40128921b7bd3a7c339e5
Cody Condon <codycondon50@gmail.com>
2016-03-28 14:52:24 -0400

    add active book reducer and wire it to booklist component

--- src/containers/book-list.js
+++ src/containers/book-list.js
@@ -2,12 +2,21 @@
 import React, {Component} from 'react';
 // import connect function from react redux, which is the glue between react and redux
 import { connect } from 'react-redux';
+import { selectBook } from '../actions/index';
+// bind action creators is a function that makes sure the action from action creator actually ends up flowing through
+// all reducers
+import { bindActionCreators} from 'redux';
 
 class BookList extends Component {
   renderList() {
     return this.props.books.map((book) => {
       return (
-        <li key={book.title} className="list-group-item">{book.title}</li>
+        <li
+         key={book.title}
+         onClick={() => this.props.selectBook(book)}
+        className="list-group-item">
+         {book.title}
+        </li>
       );
     });
   }
@@ -30,5 +39,16 @@ function mapStateToProps(state) {
     books: state.books
   };
 }
-// connect takes a function and component and produces a container
-export default connect(mapStateToProps)(BookList);
+
+//Anything returned from this function will end up as props
+// on the BookList container
+function mapDispatchToProps(dispatch) {
+  //whenever select book is called, the result should be passed
+  // to all our reducers
+  return bindActionCreators({selectBook: selectBook}, dispatch);
+}
+
+// Promote BookList from a component to a container - needs to know
+// about this new dispatch method, selectBook. Make it available
+// as a prop.
+export default connect(mapStateToProps, mapDispatchToProps)(BookList);
