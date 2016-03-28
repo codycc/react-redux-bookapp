3ab5a3ed43f1a123a4c7d14671c4cbe17c15b5fa
Cody Condon <codycondon50@gmail.com>
2016-03-28 14:57:19 -0400

    add book detail component

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,11 @@
 import { combineReducers } from 'redux';
 import  BooksReducer  from './reducer_books';
+import ActiveBook from './reducer_active_book';
 const rootReducer = combineReducers({
   //this reducer is going to add a key to our global application state called books * very important to note *
-  books: BooksReducer
+  books: BooksReducer,
+  activeBook: ActiveBook
+
 });
 
 export default rootReducer;
